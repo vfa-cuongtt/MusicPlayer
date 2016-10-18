f69374210ebe4d8bfc30952b14609097b2b16038
CuongTT <vfa.cuongtt@gmail.com>
2016-10-18 17:16:11 +0700

    PlayView play ok

--- components/playView.js
+++ components/playView.js
@@ -4,12 +4,67 @@ import {
   View,Navigator,
   Text,Image,TextInput,TouchableOpacity,ListView,ActivityIndicatorIOS
 } from 'react-native';
+var Sound = require('react-native-sound');
 
-
+var mp3;
+var selectPlay = true;
 class PlayView extends Component {
   constructor(props) {
     super(props);
-    this.state={};
+    // start when run PlayView
+    this.state={
+      imgPlay:require('../images/icon/play@2x.png')
+    };
+
+    // API play song
+    mp3 = new Sound(this.props.songObject.path , '', (error) => {
+      if(error) {
+        console.log('Failed to load sound', error);
+      } else {
+        console.log('Load sound success');
+        console.log('duration in seconds: ' + mp3.getDuration() +
+       ' number of channels: ' + mp3.getNumberOfChannels());
+      }
+    });
+
+  }
+
+  // Function: Play Song
+  playSong(){
+    if(selectPlay) {
+      mp3.play();
+      selectPlay = false;
+      this.setState({
+        imgPlay:require('../images/icon/pause@2x.png')
+      });
+    } else {
+      mp3.pause();
+      selectPlay = true;
+      this.setState({
+        imgPlay:require('../images/icon/play@2x.png')
+      });
+    }
+
+  }
+
+  //Function: Repeat Song
+  repeatSong(){
+
+  }
+
+  // Function: Rewind Song
+  rewindSong(){
+
+  }
+
+  // Function: Next Song
+  nextSong(){
+
+  }
+
+  // Function: Random Song
+  randomSong(){
+
   }
 
   render(){
@@ -35,18 +90,61 @@ class PlayView extends Component {
         </View>
 
         <View style={styles.bottom}>
-          <View style={{backgroundColor:'yellow',flex:1/3,}}>
-            <Text style={{fontSize:30}}>
+          <View style={{backgroundColor:'yellow',flex:1/10,}}>
+            <Text style={{fontSize:20,marginTop:20,paddingLeft:10}}>
               {this.props.songObject.name}
             </Text>
           </View>
-          <View style={{backgroundColor:'grey',flex:2/3,}}>
+          <View style={{backgroundColor:'grey',flex:2/4,}}>
             <Image
-              style={{width:50,height:50}}
-              source={require('../images/pikachu.jpg')}
+              source={require('../images/noo.jpg')}
             />
           </View>
-          <View style={{backgroundColor:'pink',flex:1/3,}}>
+          <View style={{backgroundColor:'pink',flex:1/4,flexDirection:'row'}}>
+            <View style={styles.soundButton}>
+              <TouchableOpacity>
+                <Image
+                  style={{width:30,height:30}}
+                  source={require('../images/icon/repeat@2x.png')}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.soundButton}>
+              <TouchableOpacity>
+              <Image
+                style={{width:30,height:30}}
+                source={require('../images/icon/rewind@2x.png')}
+              />
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.soundButton}>
+              <TouchableOpacity onPress={this.playSong.bind(this)}>
+              <Image
+                style={{width:30,height:30}}
+                source={this.state.imgPlay}
+              />
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.soundButton}>
+              <TouchableOpacity >
+              <Image
+                style={{width:30,height:30}}
+                source={require('../images/icon/next@2x.png')}
+              />
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.soundButton}>
+              <TouchableOpacity>
+              <Image
+                style={{width:30,height:30}}
+                source={require('../images/icon/random@2x.png')}
+              />
+              </TouchableOpacity>
+            </View>
 
           </View>
 
@@ -98,8 +196,10 @@ const styles = StyleSheet.create({
     borderWidth:1,
     marginTop:15
   },
-  bao:{
-    flexDirection:'row'
+  soundButton:{
+    paddingTop:50,
+    paddingLeft:20,
+    flex:1
   }
 
 });
